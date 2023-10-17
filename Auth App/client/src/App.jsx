bb413ad0e8fd1713654769c1e87c9a7bc6987871
noumanmunibe <thesmartlad27@gmail.com>
2023-10-17 17:48:13 +0500

    create header component

--- Auth App/client/src/App.jsx
+++ Auth App/client/src/App.jsx
@@ -4,19 +4,20 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import Header from "./components/Header";
 
-const App = () => {
+export default function App() {
   return (
     <BrowserRouter>
+      {/* Header */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<SignUp />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
-};
-
-export default App;
+}
