63e2b54e742ea8bd37514319252c618635e976e0
noumanmunibe <thesmartlad27@gmail.com>
2023-10-17 15:41:06 +0500

    add routes and create pages

--- Auth App/client/src/App.jsx
+++ Auth App/client/src/App.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ Auth App/client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-lg text-red-700">Welcome to the Home page!</h1>
+      <p>This is the landing page for our app.</p>
+    </div>
+  );
+};
+
+export default Home;
