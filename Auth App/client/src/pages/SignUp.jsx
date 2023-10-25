006e269ece288a4b2996beaf4c696ad2a7646275
noumanmunibe <thesmartlad27@gmail.com>
2023-10-25 23:01:50 +0500

    complete sign up page ui

--- Auth App/client/src/pages/SignUp.jsx
+++ Auth App/client/src/pages/SignUp.jsx
@@ -1,7 +1,38 @@
+import { Link } from "react-router-dom";
+
 function SignUp() {
   return (
-    <div>
-      <h1>Sign Up</h1>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+        />
+        <button className="bg-slate-700 text-white p-2 rounded-lg uppercase hover:opacity-90 disabled:opacity-80">
+          Sign Up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-3">
+        <p>Have an account already?</p>
+        <Link to="/login">
+          <span className="text-blue-700">Sign In</span>
+        </Link>
+      </div>
     </div>
   );
 }
