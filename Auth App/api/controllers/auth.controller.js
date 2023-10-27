f47e5695def6239a7213538b99c00ed968831062
noumanmunibe <thesmartlad27@gmail.com>
2023-10-28 01:47:57 +0500

    create login api route

--- Auth App/api/controllers/auth.controller.js
+++ Auth App/api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/errors.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -8,6 +10,25 @@ export const register = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ message: "User registered successfully!" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(404, "Invalid credentials!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    res
+      .cookie("access_token", token, { httpOnly: true, expiry: expiryDate })
+      .status(201)
+      .json(rest);
   } catch (err) {
     next(err);
   }
