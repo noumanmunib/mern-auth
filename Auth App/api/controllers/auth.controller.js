6fbf9b4e77ceeb12b82b25bda1e3bc06e5552101
noumanmunibe <thesmartlad27@gmail.com>
2023-10-28 01:54:46 +0500

    create login api route

--- Auth App/api/controllers/auth.controller.js
+++ Auth App/api/controllers/auth.controller.js
@@ -26,7 +26,7 @@ export const login = async (req, res, next) => {
     const { password: hashedPassword, ...rest } = validUser._doc;
     const expiryDate = new Date(Date.now() + 3600000); // 1 hour
     res
-      .cookie("access_token", token, { httpOnly: true, expiry: expiryDate })
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
       .status(201)
       .json(rest);
   } catch (err) {
