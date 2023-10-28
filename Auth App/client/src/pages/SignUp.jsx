97cf25e1bf05cd944df0d4be632defb5748a93f4
noumanmunibe <thesmartlad27@gmail.com>
2023-10-28 16:39:52 +0500

    complete the sign in page functionality

--- Auth App/client/src/pages/SignUp.jsx
+++ Auth App/client/src/pages/SignUp.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -26,6 +27,7 @@ function SignUp() {
       if (data.success === false) {
         setError(true);
       }
+      navigate("/login");
     } catch (error) {
       setLoading(false);
       setError(true);
